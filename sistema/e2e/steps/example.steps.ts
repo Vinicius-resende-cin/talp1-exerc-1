5ab91e60ac2ad74103d7a9ed8a06843ef04107fd
Vinícius Resende <vrb@cin.ufpe.br>
2026-03-24 19:09:38 -0300

    chore: remove unused dummy e2e feature and step files (#10)

--- sistema/e2e/steps/example.steps.ts
+++
@@ -1,7 +0,0 @@
-import { Given, When, Then } from "@cucumber/cucumber";
-
-Given("I have a configured cucumber project", async function () {});
-
-When("I run the tests", async function () {});
-
-Then("they should execute successfully", async function () {});
